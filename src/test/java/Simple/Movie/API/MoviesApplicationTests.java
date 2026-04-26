b17b99dcb06448c080d1419f25d4a36d00d6e13f
Justin Gutierrez <gutierrezjustin48@gmail.com>
2026-04-26 16:16:34 -0700

    Controller Tests using MockMVC and Mockito Service Tests

--- src/test/java/Simple/Movie/API/MoviesApplicationTests.java
+++ src/test/java/Simple/Movie/API/MoviesApplicationTests.java
@@ -1,7 +1,13 @@
 package Simple.Movie.API;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 class MoviesApplicationTests {
@@ -10,4 +16,20 @@ class MoviesApplicationTests {
 	void contextLoads() {
 	}
 
+}
+// tests moviecontroller getallmovies method using MockMVC for real http requests
+@WebMvcTest(MovieController.class)
+class MovieControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MovieService movieService;
+
+    @Test
+    void shouldReturnAllMovies() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/movies"))
+               .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
